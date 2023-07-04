644c11f6859491fdf4c15a437682c022082d4da7
kalio753 <duongchikien2001@gmail.com>
2023-07-04 07:43:41 +0700

    1st init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Collapse, Collapsex } from "antd"
+import "./App.css"
+import data from "./data/data"
+
+const regex = /\[(.+?)(?:\((.*?)\))?\]\((.*?)\)/
+const FIXED_DOMAIN = "https://igx.biz"
+
+data.reports.forEach((section) => {
+    section.lines.sort((a, b) => {
+        const matchA = regex.exec(a)
+        const matchB = regex.exec(b)
+        const valueA = matchA && matchA[3] ? matchA[3].replace(/^\/en/, "") : ""
+        const valueB = matchB && matchB[3] ? matchB[3].replace(/^\/en/, "") : ""
+        return valueA.localeCompare(valueB)
+    })
+})
+
+const reports = data.reports
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const items = reports.map((rp, index) => {
+        return {
+            key: index + 1,
+            label: rp.section,
+            children: (
+                <>
+                    <ol>
+                        {rp.lines.map((line) => {
+                            let title
+                            let code
+                            let link
+                            const matches = line.match(regex)
+
+                            if (matches) {
+                                title = matches[1]
+                                code = matches[2] || ""
+                                link = FIXED_DOMAIN + matches[3]
+
+                                // console.log("Title:", title)
+                                // console.log("Code:", code)
+                                // console.log("Link:", link)
+                            }
+
+                            return (
+                                <li>
+                                    <a href={link}>{title ? title : null}</a>{" "}
+                                    {code ? "(" + code + ")" : null}
+                                </li>
+                            )
+                        })}
+                    </ol>
+                </>
+            ),
+        }
+    })
+
+    return (
+        <div className="App">
+            <Collapse items={items} accordion />
+        </div>
+    )
 }
 
-export default App;
+export default App
